6aff5ba0c0c173522147619e15ce7f6be3740288
Cristiano Matos <ctoveloz@gmail.com>
2020-04-16 05:51:20 -0300

    Initial config

---
+++ index.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+
+
+	// Start QR Code Whatsapp
+	const browser = await puppeteer.launch({
+        headless: false, // No headless to scan the QR code.
+        userDataDir: 'data/userdata' // Persist the session.
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://web.whatsapp.com/');
+    await page.waitFor(5000);
+
+    console.log('Awaiting/Checking peering with WhatsApp phone');
+    await page.waitFor('#side', { timeout: 60000 }).then(() => { // Scan the QR code within the next minute.
+        console.log('Connected !');
+    }).catch((res) => {
+        console.log('Not connected !', res);
+        return -1;
+    })
+    await page.waitFor(1000);
+
+
+    //Find Group
+	await page.focus('._2S1VP'); // Focus search input form.
+    await page.keyboard.type('Testeee', { delay: 100 });
+    await page.waitFor(6000);
+
+    // Select the best result.
+    let contactElt = (await page.$x(`//*[@class="_25Ooe" and . = "Testeee"]`))[0];
+    contactElt.click();
+    await page.waitFor(1000);
+
+    // Select Group Status
+    await page.click('._5SiUq');
+    await page.waitFor(1000);
+
+    //Select Locations Tab
+    const handles = await page.$$('._2phEY');
+		for (const handle of handles)
+  	await handle.click();
+  	await page.waitFor(1000);
+
+  	//Select Users Tracker
+  	await page.click('._V9yxQ');
+
+})();
